a1e7c3a6a980f3dc34c259dad5adb04251178eec
herdi34 <herdi13yyo@gmail.com>
2022-03-03 14:26:46 +0700

    Pindah Fragment

--- app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.kt
@@ -9,27 +9,23 @@ import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-class HomeFragment : Fragment(), View.OnClickListener {
+class CategoryFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCategory: Button = view.findViewById(R.id.btn_category)
-        btnCategory.setOnClickListener(this)
+        val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
     }
+
     override fun onClick(v: View) {
-        if (v.id == R.id.btn_category) {
-            val mCategoryFragment = CategoryFragment()
-            val mFragmentManager = parentFragmentManager
-            mFragmentManager.beginTransaction().apply {
-                replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
-                commit()
-            }
+        if (v.id == R.id.btn_detail_category) {
+
         }
     }
 }
