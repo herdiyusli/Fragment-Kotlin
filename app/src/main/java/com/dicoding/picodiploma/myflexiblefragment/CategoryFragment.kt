daadeb805dea37fb35657fd66fc091a6db9ecce2
herdi34 <herdi13yyo@gmail.com>
2022-03-03 15:20:01 +0700

    Pindah Fragment mengirim data pada fragment

--- app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.kt
@@ -11,6 +11,7 @@ import android.widget.Button
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 class CategoryFragment : Fragment(), View.OnClickListener {
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -19,13 +20,27 @@ class CategoryFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        val btnDetailCategory:Button = view.findViewById(R.id.btn_detail_category)
         btnDetailCategory.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
         if (v.id == R.id.btn_detail_category) {
+            val mDetailCategoryFragment = DetailCategoryFragment()
 
+            val mBundle = Bundle()
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle")
+            val description = "Kategori ini akan berisi produk-produk lifestyle"
+
+            mDetailCategoryFragment.arguments = mBundle
+            mDetailCategoryFragment.description = description
+
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager?.beginTransaction()?.apply {
+                replace(R.id.frame_container, mDetailCategoryFragment, DetailCategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
         }
     }
 }
